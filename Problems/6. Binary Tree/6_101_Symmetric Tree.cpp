caf290ff42dda2fa5a4c4ef7aba2d027c838ee17
Hongyi Luo <roland.law.k@gmail.com>
2022-08-09 14:58:57 +0200

    update

---
+++ Problems/6. Binary Tree/6_101_Symmetric Tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        queue<TreeNode*> que;
+        que.push(root->left);
+        que.push(root->right);
+        
+        while(!que.empty()){
+            TreeNode* leftNode = que.front(); que.pop();
+            TreeNode* rightNode = que.front(); que.pop();
+            
+            if(leftNode == NULL && rightNode == NULL){
+                continue;
+            }
+            if(!leftNode || !rightNode || (leftNode->val != rightNode->val)){
+                return false;
+            }
+            que.push(leftNode->left);
+            que.push(rightNode->right);
+            que.push(leftNode->right);
+            que.push(rightNode->left);
+        }
+        return true;
+    }
+};
---
+++ Problems/6. Binary Tree/8_111_Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    int minDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        int depth = 0;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            depth++; 
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+                if (!node->left && !node->right) { 
+                    return depth;
+                }
+            }
+        }
+        return depth;
+    }
+};
